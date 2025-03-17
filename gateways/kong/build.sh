c32ee8c383f220a7f7e8708ff1808498f7872433
edgars <edgar@skalena.com>
2025-03-17 14:47:21 -0300

    trinda rocks

---
+++ gateways/kong/build.sh
@@ -0,0 +1,6 @@
+docker build -t edgars/kong-gw-sample .
+docker push edgars/kong-gw-sample
+
+#docker run -p 8000:8000 edgars/kong-gw-sample
+
+#docker build --platform linux/amd64 -t edgars/kong-gw-sample . 
